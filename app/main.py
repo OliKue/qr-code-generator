109edd162939cf180a8b67a0459f0872c8c40a70
OliKue <oliver.kuetemeier123@gmail.com>
2024-03-06 09:39:20 +0100

    running in docker

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, send_file, jsonify
+import qrcode
+from io import BytesIO
 
 app = Flask(__name__)
+
+@app.route('/generate_qrcode/<data>')
+def generate_qrcode(data):
+    # Generate QR code
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    # Create an in-memory bytes stream to store the QR code image
+    qr_img_stream = BytesIO()
+    qr.make_image(fill_color="black", back_color="white").save(qr_img_stream, 'PNG')
+    qr_img_stream.seek(0)
+
+    # Return the QR code image via REST
+    return send_file(qr_img_stream, mimetype='image/png')
 
 
 @app.route("/hello", methods=["GET"])
